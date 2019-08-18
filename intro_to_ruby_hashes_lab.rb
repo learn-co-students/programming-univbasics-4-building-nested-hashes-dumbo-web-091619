bd66ad4a4e128bf03682039a306c3791a7f8d61d
Chen Wang <wangcees@gmail.com>
2019-08-18 19:29:52 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,16 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	monopoly = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
+  new_hash = base_hash
+  if new_hash[:railroads][:pieces]
+  else
+    new_hash[:railroads][:pieces] = 4
+  end
+  new_hash
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -12,6 +18,24 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  new_hash = monopoly_with_second_tier
+  if new_hash[:railroads][:rent_in_dollars]
+  else
+    new_hash[:railroads][:rent_in_dollars] = {
+      :four_pieces_owned => 200,
+      :three_pieces_owned => 100,
+      :two_pieces_owned => 50,
+      :one_piece_owned => 25,
+    }
+    new_hash[:railroads][:names] = {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      :b_and_o_railroad => {},
+      :shortline_railroad => {}
+    }
+  
+  end
+  new_hash 
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -19,6 +43,15 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+  new_hash = monopoly_with_third_tier
+  if  new_hash[:railroads][:names][:reading_railroad]["mortgage_value"]
+  else
+    new_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+    new_hash[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
+    new_hash[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+    new_hash[:railroads][:names][:shortline_railroad]["mortgage_value"] = "$800"
+  end
+  new_hash
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
